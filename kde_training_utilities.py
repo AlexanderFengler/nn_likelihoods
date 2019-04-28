b17ebbf5d7464121e1ac7a33a3ab90b182ff02e0
alex <alexanderfengler@gmx.de>
2019-04-28 18:50:13 -0400

    kde_utilities_update

--- kde_training_utilities.py
+++ kde_training_utilities.py
@@ -95,6 +95,85 @@ def generate_training_base_kde(file_name = '',
     print('Finished ' + file_name)
     print('Files generated at this point: ', len(os.listdir(to_folder)))
 
+def generate_training_base_kde_flexbound(file_name = '',
+                                           from_folder = '',
+                                           to_folder = '',
+                                           n_samples_by_kde = 10000,
+                                           mixture_p = [0.8, 0.1, 0.1], # [proportion from process, proportion low uniform, proportion high uniform]
+                                           model = 'ddm_flexbound',
+                                           print_info = False,
+                                           target_file_format = 'pickle' # at this point can be 'pickle', 'csv'
+                                           ): 
+    
+    # currently just first 10 files for testing
+    
+    # REWRITE HERE: INSTEAD OF FOR LOOP DEFINE THIS FUNCTION SUCH THAT IT EXECUTES FOR A SINGLE FILE ONLY
+    # 'fid',
+    my_columns = ['v', 'a', 'w', 'c1', 'c2', 'rt', 'choice', 'log_l', 'kde_type', 'bandwidth_rule' , 'mixture', 'model']
+    tmp_data = pd.DataFrame(np.zeros((n_samples_by_kde, len(my_columns))),
+                            columns = my_columns)
+    
+    n_kde = int(n_samples_by_kde * mixture_p[0])
+    n_unif_up = int(n_samples_by_kde * mixture_p[1])
+    n_unif_down = int(n_samples_by_kde * mixture_p[2])
+    tmp_sim_data = pickle.load( open( from_folder + '/' + file_name, "rb" ) )
+    
+    tmp_data.iloc[:, my_columns.index('v')] = tmp_sim_data[2]['v'] 
+    tmp_data.iloc[:, my_columns.index('a')] = tmp_sim_data[2]['a']
+    tmp_data.iloc[:, my_columns.index('w')] = tmp_sim_data[2]['w']
+    tmp_data.iloc[:, my_columns.index('c1')] = tmp_sim_data[2]['c1']
+    tmp_data.iloc[:, my_columns.index('c2')] = tmp_sim_data[2]['c2']
+    tmp_data.iloc[:, my_columns.index('kde_type')] = 'gaussian'
+    tmp_data.iloc[:, my_columns.index('bandwidth_rule')] = 'silverman'
+    tmp_data.iloc[:, my_columns.index('mixture')] = str(mixture_p)
+    tmp_data.iloc[:, my_columns.index('model')] = model
+    #tmp_data.iloc[:, my_columns.index('fid')] = file_name
+    
+    # Get simulated data from kde
+    tmp_kde = kde_class.logkde(tmp_sim_data)
+    tmp_kde_samples = tmp_kde.kde_sample(n_samples = n_kde)
+
+    tmp_data.iloc[:n_kde, my_columns.index('rt')] = tmp_kde_samples[0]
+    tmp_data.iloc[:n_kde, my_columns.index('choice')] = tmp_kde_samples[1]
+    tmp_data.iloc[:n_kde, my_columns.index('log_l')] = tmp_kde.kde_eval(data = tmp_kde_samples)
+
+    # Generate rest of the data (to make sure we observe outliers that are negative and close to max_rt)
+    # Negative uniform part
+    choice_tmp = np.random.choice(tmp_sim_data[2]['possible_choices'], 
+                                  size = n_unif_down)
+    rt_tmp = np.random.uniform(low = -1, 
+                               high = 0.0001, 
+                               size = n_unif_down)
+
+    tmp_data.iloc[n_kde:(n_kde + n_unif_down), my_columns.index('rt')] = rt_tmp
+    tmp_data.iloc[n_kde:(n_kde + n_unif_down), my_columns.index('choice')] = choice_tmp
+    tmp_data.iloc[n_kde:(n_kde + n_unif_down), my_columns.index('log_l')] = -66.77497 # the number corresponds to log(1e-29)
+
+    # Positive uniform part
+    choice_tmp = np.random.choice(tmp_sim_data[2]['possible_choices'], 
+                                  size = n_unif_up)
+    rt_tmp = np.random.uniform(low = 0.0001, 
+                               high = tmp_sim_data[2]['max_t'], 
+                               size = n_unif_up)
+
+    tmp_data.iloc[(n_kde + n_unif_down): , my_columns.index('rt')] = rt_tmp
+    tmp_data.iloc[(n_kde + n_unif_down): , my_columns.index('choice')] = choice_tmp
+    tmp_data.iloc[(n_kde + n_unif_down):, my_columns.index('log_l')] = tmp_kde.kde_eval(data = (rt_tmp, choice_tmp))
+    
+    # Some code to get the exact right files name
+    # TO BE FIXED : (uuid needs to be separated from 'seconds' component of file name in the 'base_simulations' folder
+    
+    # Write to file
+    if target_file_format == 'csv':
+        tmp_data.to_csv(to_folder + '/kde_' + model + '_' + file_name[-39:-7] + '.csv', index = False)
+    if target_file_format == 'pickle':
+        tmp_data.to_pickle(to_folder + '/kde_' + model + '_' + file_name[-39:-7] + '.pickle')
+    
+    # Print statements
+    print('Finished ' + file_name)
+    print('Files generated at this point: ', len(os.listdir(to_folder)))
+
+    
 def kde_shuffle_data(from_folder = '',
                      to_folder = '', # note: if we skip copying, the to folder
                      file_chunk_size = 1000,
