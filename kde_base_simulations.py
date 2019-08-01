a284aaaf5170a535b5ed3c8e2716621e943f8920
Alex <afengler@gmail.com>
2019-08-01 13:02:48 -0400

    ..

--- kde_base_simulations.py
+++ kde_base_simulations.py
@@ -23,10 +23,10 @@ import boundary_functions as bf
 
 def data_generator(*args):
     # CHOOSE SIMULATOR HERE
-    simulator_data = ds.full_ddm(*args)
+    simulator_data = ds.ddm_flexbound(*args)
     
     # CHOOSE TARGET DIRECTORY HERE
-    file_dir =  '/users/afengler/data/kde/full_ddm/base_simulations_20000/'
+    file_dir =  '/users/afengler/data/kde/ddm/base_simulations_20000/'
 
     # STORE
     file_name = file_dir + simulator + '_' + uuid.uuid1().hex
@@ -38,13 +38,13 @@ if __name__ == "__main__":
     n_cpus = psutil.cpu_count(logical = False)
 
     # Parameter ranges (for the simulator)
-    v = [-1.5, 1.5]
+    v = [-2, 2]
     w = [0.3, 0.7]
     a = [0.3, 2]
     
     # FULL DDM
-    dw = [0.0, 0.1]
-    sdv = [0.0, 0.5]
+    #dw = [0.0, 0.1]
+    #sdv = [0.0, 0.5]
 
     #     c1 = [0, 5]
 #     c2 = [1, 1.5]
@@ -58,16 +58,16 @@ if __name__ == "__main__":
 #     scale = [0.1, 10]
 
     # Simulator parameters
-    simulator = 'full_ddm'
+    simulator = 'ddm_flexbound'
     s = 1
     delta_t = 0.01
-    max_t = 20
+    max_t = 30
     n_samples = 20000
     print_info = False
     boundary_multiplicative = False
 
     # Number of simulators to run
-    n_simulators = 2500
+    n_simulators = 10000
 
     # Make function input tuples
     v_sample = np.random.uniform(low = v[0], high = v[1], size = n_simulators)
@@ -75,8 +75,8 @@ if __name__ == "__main__":
     a_sample = np.random.uniform(low = a[0], high = a[1], size = n_simulators)
 
     # Full DDM
-    dw_sample = np.random.uniform(low = dw[0], high = dw[1], size = n_simulators)
-    sdv_sample = np.random.uniform(low = sdv[0], high = sdv[1], size = n_simulators)
+    #dw_sample = np.random.uniform(low = dw[0], high = dw[1], size = n_simulators)
+    #sdv_sample = np.random.uniform(low = sdv[0], high = sdv[1], size = n_simulators)
     
     # Exp c1_c2
 #     c1_sample = np.random.uniform(low = c1[0], high = c1[1], size = n_kdes)
@@ -99,8 +99,8 @@ if __name__ == "__main__":
         args_list.append((v_sample[i], # CHOOSE PARAM
                           a_sample[i], # CHOOSE PARAM
                           w_sample[i], ## ....
-                          dw_sample[i], # CHOOSE PARAM
-                          sdv_sample[i], # CHOOSE PARAM
+                          #dw_sample[i], # CHOOSE PARAM
+                          #sdv_sample[i], # CHOOSE PARAM
                           s,
                           delta_t,
                           max_t,
