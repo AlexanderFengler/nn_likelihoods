8939af79cb6ddc37a8dda39aeb177c1c856d25ab
Alex <afengler@gmail.com>
2019-08-03 14:55:13 -0400

    ..

--- kde_base_simulations.py
+++ kde_base_simulations.py
@@ -23,10 +23,10 @@ import boundary_functions as bf
 
 def data_generator(*args):
     # CHOOSE SIMULATOR HERE
-    simulator_data = ds.ddm_flexbound(*args)
+    simulator_data = ds.full_ddm(*args)
     
     # CHOOSE TARGET DIRECTORY HERE
-    file_dir =  '/users/afengler/data/kde/linear_collapse/base_simulations_20000/'
+    file_dir =  '/users/afengler/data/kde/full_ddm/base_simulations_20000/'
 
     # STORE
     file_name = file_dir + simulator + '_' + uuid.uuid1().hex
@@ -43,14 +43,14 @@ if __name__ == "__main__":
     a = [0.5, 2]
     
     # FULL DDM
-    #dw = [0.0, 0.1]
-    #sdv = [0.0, 0.5]
+    dw = [0.0, 0.1]
+    sdv = [0.0, 0.5]
 
     #     c1 = [0, 5]
 #     c2 = [1, 1.5]
     # Linear Collapse
-    node = [0, 2]
-    theta = [0, np.pi/2 - 0.2]
+#     node = [0, 2]
+#     theta = [0, np.pi/2 - 0.2]
 
     # Weibull Bound
 #     node = [0, 5]
@@ -58,16 +58,16 @@ if __name__ == "__main__":
 #     scale = [0.1, 10]
 
     # Simulator parameters
-    simulator = 'ddm_flexbound'
+    simulator = 'full_ddm'
     s = 1
     delta_t = 0.01
     max_t = 30
     n_samples = 20000
     print_info = False
-    boundary_multiplicative = False # CHOOSE WHETHER BOUNDARY IS MULTIPLICATIVE (W.R.T Starting separation) OR NOT
+    boundary_multiplicative = True # CHOOSE WHETHER BOUNDARY IS MULTIPLICATIVE (W.R.T Starting separation) OR NOT
 
     # Number of simulators to run
-    n_simulators = 3096
+    n_simulators = 50000
 
     # Make function input tuples
     v_sample = np.random.uniform(low = v[0], high = v[1], size = n_simulators)
@@ -75,16 +75,16 @@ if __name__ == "__main__":
     a_sample = np.random.uniform(low = a[0], high = a[1], size = n_simulators)
 
     # Full DDM
-    #dw_sample = np.random.uniform(low = dw[0], high = dw[1], size = n_simulators)
-    #sdv_sample = np.random.uniform(low = sdv[0], high = sdv[1], size = n_simulators)
+    dw_sample = np.random.uniform(low = dw[0], high = dw[1], size = n_simulators)
+    sdv_sample = np.random.uniform(low = sdv[0], high = sdv[1], size = n_simulators)
     
     # Exp c1_c2
 #     c1_sample = np.random.uniform(low = c1[0], high = c1[1], size = n_simulators)
 #     c2_sample = np.random.uniform(low = c2[0], high = c2[1], size = n_simulators)
 
     # Linear Collapse
-    node_sample = np.random.uniform(low = node[0], high = node[1], size = n_simulators)
-    theta_sample = np.random.uniform(low = theta[0], high = theta[1], size = n_simulators)
+#     node_sample = np.random.uniform(low = node[0], high = node[1], size = n_simulators)
+#     theta_sample = np.random.uniform(low = theta[0], high = theta[1], size = n_simulators)
 
     # Weibull
 #     node_sample = np.random.uniform(low = node[0], high = node[1], size = n_simulators)
@@ -99,17 +99,16 @@ if __name__ == "__main__":
         args_list.append((v_sample[i], # CHOOSE PARAM
                           a_sample[i], # CHOOSE PARAM
                           w_sample[i], ## ....
-                          #dw_sample[i], # CHOOSE PARAM
-                          #sdv_sample[i], # CHOOSE PARAM
+                          dw_sample[i], # CHOOSE PARAM
+                          sdv_sample[i], # CHOOSE PARAM
                           s,
                           delta_t,
                           max_t,
                           n_samples,
                           print_info,
-                          bf.linear_collapse, # CHOOSE: BOUNDARY FUNCTION
+                          bf.constant, # CHOOSE: BOUNDARY FUNCTION
                           boundary_multiplicative, # CHOOSE: IS BOUNDARY MULTIPLICATIVE?
-                          {'node': node_sample[i],
-                          'theta': theta_sample[i]}))
+                          {}))
 
     # Parallel Loop
     with Pool(processes = n_cpus) as pool:
