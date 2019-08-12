db9094f0ea86059671b27e9f8c9f2ea16a9da926
Alex <afengler@gmail.com>
2019-08-11 20:35:59 -0400

    ..

--- dnn_train_eval.py
+++ dnn_train_eval.py
@@ -8,63 +8,23 @@ import time
 from datetime import datetime
 import csv
 import os
-
-# Importing helper functions to make data set (CHANGE WHEN CHANGING DATA GENERATING MODEL)
-# Import make_data_sin as mds
-import make_data_wfpt as mdw
-import make_data_sin as mds
+import yaml
 
 class dnn_trainer():
 
     # Getting working directory in case it is useful later (definition of model_dir)
     cwd = os.getcwd()
 
-    def __init__(self):
-        # get currecnt data and time for file naming purposes
-        self.timestamp = datetime.now().strftime('%m_%d_%y_%H_%M_%S')
-
-        self.model_params = {
-                            'input_shape': 3,
-                            'output_shape': 1,
-                            'output_activation': 'sigmoid',
-                            'hidden_layers': [20, 20, 20, 20],
-                            'hidden_activations': ['relu', 'relu', 'relu', 'relu'],
-                            'l1_activation': [0.0, 0.0, 0.0, 0.0],
-                            'l2_activation': [0.0, 0.0, 0.0, 0.0],
-                            'l1_kernel': [0.0, 0.0, 0.0, 0.0],
-                            'l2_kernel': [0.0, 0.0, 0.0, 0.0],
-                            'optimizer': 'Nadam',
-                            'loss': 'mse',
-                            'metrics': ['mse']
-                            }
-
-        self.train_params = {
-                            'callback_funs': ['ReduceLROnPlateau', 'EarlyStopping', 'ModelCheckpoint'],
-                            'plateau_patience': 10,
-                            'min_delta': 1e-4, # Minimum improvement in evaluation metric that counts as learning
-                            'early_stopping_patience': 15,
-                            'callback_monitor': 'loss',
-                            'min_learning_rate': 1e-7,
-                            'red_coef_learning_rate': 0.1,
-                            'ckpt_period': 10,
-                            'ckpt_save_best_only': True,
-                            'ckpt_save_weights_only': True,
-                            'max_train_epochs': 2000,
-                            'batch_size': 10000,
-                            'warm_start': False,
-                            'checkpoint': 'ckpt',
-                            'model_cnt': 0  # This is important for saving result files coherently, a global cnt of models run thus far
-                            }
-
-        self.data_params = {
-                           'data_type': 'choice_probabilities',
-                           'model_directory': self.cwd + '/keras_models',
-                           'checkpoint': 'ckpt',
-                           'model_name': 'dnnregressor',
-                           'data_type_signature': '_choice_probabilities_analytic_',
-                           'timestamp': self.timestamp,
-                           'training_data_size': 2500000
-                           }
+    def __init__(self, yaml_config_file_path = 'file_path'):
+        
+        # Read configuration file
+        with open(yaml_config_file_path, 'r') as stream: 
+            config_data = yaml.unsafe_load(stream) 
+        
+        self.model_params = config_data['model_params']
+        self.train_params = config_data['train_params']
+        self.data_params = config_data['data_params']
+        self.data_params['timestamp'] = datetime.now().strftime('%m_%d_%y_%H_%M_%S')
 
         self.model  = []
         self.data = {}
@@ -74,6 +34,11 @@ class dnn_trainer():
                              self,
                              save_model = True
                              ):
+        
+        # Refresh some data-structures now that data is here
+        self.data_params['training_data_size'] = self.data['train_features'].shape[0]
+        self.model_params['input_shape'] = self.data['train_features'].shape[1]
+        self.model_params['output_shape'] = self.data['test_labels'].shape[1]
 
         # This returns a tensor
         inputs = keras.layers.Input(shape = (self.model_params['input_shape'], ))
@@ -137,7 +102,6 @@ class dnn_trainer():
                      ):
 
         # get currecnt data and time for file naming purposes
-        #date_time_str = datetime.now().strftime('%m_%d_%y_%H_%M_%S')
         model_cnt_tmp = str(self.train_params['model_cnt'])
 
         # get set of callback functions to consider dependent on the train parameters specified
@@ -271,3 +235,49 @@ class dnn_trainer():
 
             writer = csv.writer(f)
             writer.writerow(values)
+            
+            
+            
+# NOT USED:         
+#         self.model_params = {
+#                             'input_shape': 3,
+#                             'output_shape': 1,
+#                             'output_activation': 'sigmoid',
+#                             'hidden_layers': [20, 20, 20, 20],
+#                             'hidden_activations': ['relu', 'relu', 'relu', 'relu'],
+#                             'l1_activation': [0.0, 0.0, 0.0, 0.0],
+#                             'l2_activation': [0.0, 0.0, 0.0, 0.0],
+#                             'l1_kernel': [0.0, 0.0, 0.0, 0.0],
+#                             'l2_kernel': [0.0, 0.0, 0.0, 0.0],
+#                             'optimizer': 'Nadam',
+#                             'loss': 'mse',
+#                             'metrics': ['mse']
+#                             }
+
+#         self.train_params = {
+#                             'callback_funs': ['ReduceLROnPlateau', 'EarlyStopping', 'ModelCheckpoint'],
+#                             'plateau_patience': 10,
+#                             'min_delta': 1e-4, # Minimum improvement in evaluation metric that counts as learning
+#                             'early_stopping_patience': 15,
+#                             'callback_monitor': 'loss',
+#                             'min_learning_rate': 1e-7,
+#                             'red_coef_learning_rate': 0.1,
+#                             'ckpt_period': 10,
+#                             'ckpt_save_best_only': True,
+#                             'ckpt_save_weights_only': True,
+#                             'max_train_epochs': 2000,
+#                             'batch_size': 10000,
+#                             'warm_start': False,
+#                             'checkpoint': 'ckpt',
+#                             'model_cnt': 0  # This is important for saving result files coherently, a global cnt of models run thus far
+#                             }
+
+#         self.data_params = {
+#                            'data_type': 'choice_probabilities',
+#                            'model_directory': self.cwd + '/keras_models',
+#                            'checkpoint': 'ckpt',
+#                            'model_name': 'dnnregressor',
+#                            'data_type_signature': '_choice_probabilities_analytic_',
+#                            'timestamp': self.timestamp,
+#                            'training_data_size': 2500000
+#                            }
