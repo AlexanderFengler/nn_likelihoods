19af9a9f861c232427b514a907756be2244695cf
Alex <afengler@gmail.com>
2019-09-24 20:16:52 -0400

    ..

--- sbatch_sim_stats.sh
+++ sbatch_sim_stats.sh
@@ -5,22 +5,27 @@
 # job name:
 ##SBATCH -J ornstein_sim_stats
 ##SBATCH -J ddm_sim_stats
+#SBATCH -J ddm_ndt_sim_stats
 ##SBATCH -J lin_col_stats
 ##SBATCH -J full_ddm_sim_stats
 ##SBATCH -J weibull_sim_stats
-#SBATCH -J lba_sim_stats
+#@SBATCH -J lba_sim_stats
+
+# priority
+#SBATCH --account=bibs-frankmj-condo
 
 # output file
 ##SBATCH --output /users/afengler/batch_job_out/ddm_sim_stats_%A_%a.out
+#SBATCH --output /users/afengler/batch_job_out/ddm_ndt_sim_stats_%A_%a.out
 ##SBATCH --output /users/afengler/batch_job_out/full_ddm_sim_stats_%A_%a.out
 ##SBATCH --output /users/afengler/batch_job_out/ornstein_sim_stats_%A_%a.out
 ##SBATCH --output /users/afengler/batch_job_out/weibull_sim_stats_%A_%a.out
 ##SBATCH --output /users/afengler/batch_job_out/lin_col_sim_stats_%A_%a.out
-#SBATCH --output /users/afengler/batch_job_out/lba_col_sim_stats_%A_%a.out
+##SBATCH --output /users/afengler/batch_job_out/lba_col_sim_stats_%A_%a.out
 
 # Request runtime, memory, cores:
 #SBATCH --time=15:00:00
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH -c 14
 #SBATCH -N 1
 #SBATCH --array=1-1
