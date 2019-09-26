26d6798b2257a0a9ad02c335e4b06a8c349f3993
Alex <afengler@gmail.com>
2019-09-25 20:55:49 -0400

    ..

--- sbatch_sim_stats.sh
+++ sbatch_sim_stats.sh
@@ -3,27 +3,13 @@
 # Default resources are 1 core with 2.8GB of memory per core.
 
 # job name:
-##SBATCH -J ornstein_sim_stats
-##SBATCH -J ddm_sim_stats
-##SBATCH -J ddm_ndt_sim_stats
-##SBATCH -J lin_col_stats
-##SBATCH -J full_ddm_sim_stats
-##SBATCH -J weibull_sim_stats
-##SBATCH -J lba_sim_stats
-#SBATCH -J lba_ndt_sim_stats
+#SBATCH -J angle_ndt_sim_stats
 
 # priority
 #SBATCH --account=bibs-frankmj-condo
 
 # output file
-##SBATCH --output /users/afengler/batch_job_out/ddm_sim_stats_%A_%a.out
-##SBATCH --output /users/afengler/batch_job_out/ddm_ndt_sim_stats_%A_%a.out
-##SBATCH --output /users/afengler/batch_job_out/full_ddm_sim_stats_%A_%a.out
-##SBATCH --output /users/afengler/batch_job_out/ornstein_sim_stats_%A_%a.out
-##SBATCH --output /users/afengler/batch_job_out/weibull_sim_stats_%A_%a.out
-##SBATCH --output /users/afengler/batch_job_out/lin_col_sim_stats_%A_%a.out
-#SBATCH --output /users/afengler/batch_job_out/lba_ndt_sim_stats_%A_%a.out
-##SBATCH --output /users/afengler/batch_job_out/lba_col_sim_stats_%A_%a.out
+##SBATCH --output /users/afengler/batch_job_out/angle_ndt_sim_stats_%A_%a.out
 
 # Request runtime, memory, cores:
 #SBATCH --time=24:00:00
