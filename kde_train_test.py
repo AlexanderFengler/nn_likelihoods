6e1177c865fed547b000e9609fc5d3481457c39a
Alex <afengler@gmail.com>
2019-09-25 21:19:41 -0400

    ..

--- kde_train_test.py
+++ kde_train_test.py
@@ -16,11 +16,15 @@ import kde_class as kde
 if __name__ == "__main__":
 
     # PICK
-    base_simulation_folder = '/users/afengler/data/kde/lba/base_simulations_ndt_20000/'
-    target_folder = '/users/afengler/data/kde/lba/train_test_data_ndt_20000'
+    base_simulation_folder = '/users/afengler/data/kde/ddm/base_simulations_ndt_20000/'
+    target_folder = '/users/afengler/data/kde/ddm/train_test_data_ndt_20000'
     
     # LBA
-    process_params = ['v_0', 'v_1', 'A', 'b', 's', 'ndt']
+#     process_params = ['v_0', 'v_1', 'A', 'b', 's', 'ndt']
+#     files_ = pickle.load(open(base_simulation_folder + 'keep_files.pickle', 'rb'))
+
+    # DDM NDT
+    process_params = ['v', 'a', 'w', 'ndt']
     files_ = pickle.load(open(base_simulation_folder + 'keep_files.pickle', 'rb'))
 
     if not os.path.isdir(target_folder):
