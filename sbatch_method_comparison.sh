1cf3727b7cc72d1c537a8120c5f90925ffefea66
Alex <afengler@gmail.com>
2019-10-14 21:39:30 -0400

    ..

--- sbatch_method_comparison.sh
+++ sbatch_method_comparison.sh
@@ -14,8 +14,8 @@
 # Request runtime, memory, cores:
 #SBATCH --time=24:00:00
 #SBATCH --mem=128G
-#SBATCH -c 54
-#SBATCH -N 8
+#SBATCH -c 12
+#SBATCH -N 1
 ##SBATCH -p gpu --gres=gpu:1
 #SBATCH --array=1-1
 
