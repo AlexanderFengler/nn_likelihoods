4aacc8a0b2e2ad56f231df57978a931cd472a682
Alex <afengler@gmail.com>
2019-10-20 15:42:58 -0400

    ..

--- bin_data.py
+++ bin_data.py
@@ -49,7 +49,7 @@ def bin_simulator_output(out = [0, 0],
         counts[i] =  np.asmatrix(counts[i]).T
 
     label = np.concatenate(counts, axis = 1)
-    features = [out[2]['v'], out[2]['a'], out[2]['w']], out[2]['ndt']]
+    features = [out[2]['v'], out[2]['a'], out[2]['w'], out[2]['ndt']]
     return (features, label)
 
 files_ = os.listdir('/users/afengler/data/kde/ddm/base_simulations_ndt_20000')
