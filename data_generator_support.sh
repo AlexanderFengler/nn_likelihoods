c294d62c66d5a6c2d4afd630be17fe2f443da306
Alexander <alexanderfengler@gmx.de>
2019-11-03 13:09:42 -0500

    ..

---
+++ data_generator_support.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+n_data_points=( 100 250 500 1000 2000 3000 )
+#file_id=( 1 2 3 4 5 6 7 8 9 10 )
+n_experiments=1
+# outer -------------------------------------
+#for n in "${n_data_points[@]}"
+#do 
+# inner ---------------------------------
+#for id in "${file_id[@]}"
+for id in {1..100}
+do
+    python dataset_generator.py ccv weibull_cdf_ndt uniform 10 1 2500 $id
+#         echo $n
+#         echo $id
+done
+# ---------------------------------------
+#done
+# -------------------------------------------
