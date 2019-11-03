22c8c3da0e770850fbb275148dd6deabbb0fa157
Alexander <alexanderfengler@gmx.de>
2019-11-03 15:20:46 -0500

    ..

--- sbatch_base_simulations.sh
+++ sbatch_base_simulations.sh
@@ -19,4 +19,4 @@
 #SBATCH --array=1-100
 
 # Run a command
-python -u /users/afengler/git_repos/nn_likelihoods/kde_base_simulations.py ccv race_model 3 100000 1 $SLURM_ARRAY_TASK_ID
+python -u /users/afengler/git_repos/nn_likelihoods/kde_base_simulations.py ccv race_model 4 100000 1 $SLURM_ARRAY_TASK_ID
