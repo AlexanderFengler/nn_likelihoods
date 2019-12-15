1fe982a29157ae1657c3400059e75ba209197aff
Alexander <alexanderfengler@gmx.de>
2019-12-15 00:02:42 -0500

    ..

--- dependent_jobs_test.sh
+++ dependent_jobs_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get base simulations
-jobID_1=$(sbatch  sbatch_base_simulations.sh | cut -f 4 -d' ')
+jobID_1=$(sbatch sbatch_base_simulations.sh | cut -f 4 -d' ')
 
 # Get simulator stats
 jobID_2=$(sbatch --dependency=afterok:$jobID_1 sbatch_sim_stats.sh | cut -f 4 -d' ')
 
 # Make training data for filtered base simulations
-sbatch  --dependency=afterany:$jobID_2 sbatch_kde_to_base_simulations.sh
+jobID_3=$(sbatch --dependency=afterok:$jobID_2 sbatch_kde_to_base_simulations.sh | cut -f 4 -d' ')
