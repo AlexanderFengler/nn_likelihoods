59779b07ff7b75fdcfc95dd0762d40570e2a44d3
Alexander <alexanderfengler@gmx.de>
2020-03-28 01:25:21 -0400

    ..

--- sbatch_train_mlp.sh
+++ sbatch_train_mlp.sh
@@ -35,7 +35,8 @@ conda activate tf-gpu-py37
 nfiles=100
 method='levy'
 machine='ccv'
-python -u /users/afengler/git_repos/nn_likelihoods/keras_fit_model.py --machine $machine --method $method --nfiles $nfiles --datafolder /media/data_cifs/afengler/data/kde/${method}/training_data_binned_0_nbins_0_n_20000/ --nbydataset 10000000 --warmstart 0
+#data_folder='/users/afengler/data/kde/levy/'
+python -u /users/afengler/git_repos/nn_likelihoods/keras_fit_model.py --machine $machine --method $method --nfiles $nfiles --datafolder /users/afengler/data/kde/${method}/training_data_binned_0_nbins_0_n_20000/ --nbydataset 10000000 --warmstart 0
 
 
 
