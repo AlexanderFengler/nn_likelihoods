8c9aec195c34302bf9099e4b4c30a22ab1cc3ee7
Alexander <alexanderfengler@gmx.de>
2020-03-27 23:13:24 -0400

    ..

--- sbatch_train_mlp.sh
+++ sbatch_train_mlp.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
-# Pick number of files to consider
-nfiles=100
-method='ornstein'
+# Default resources are 1 core with 2.8GB of memory per core.
 
-# Function call
-python -u keras_fit_model.py --machine x7 --method $method --nfiles $nfiles --datafolder /media/data_cifs/afengler/data/kde/${method}/training_data_binned_0_nbins_0_n_20000/ --nbydataset 10000000 --warmstart 0
+# job name:
+#SBATCH -J gpu_test
+
+# priority
+##SBATCH --account=bibs-frankmj-condo
+#SBATCH --account=carney-frankmj-condo
+
+# email error reports
+#SBATCH --mail-user=alexander_fengler@brown.edu 
+#SBATCH --mail-type=ALL
+
+# output file
+#SBATCH --output /users/afengler/batch_job_out/gpu_test.out
+
+# Request runtime, memory, cores
+#SBATCH --time=2:00:00
+#SBATCH --mem=256G
+#SBATCH -c 14
+#SBATCH -N 1
+#SBATCH --constraint='quadrortx'
+##SBATCH --constraint='cascade'
+#SBATCH -p gpu --gres=gpu:1
+##SBATCH --array=1-1
+
+conda deactivate
+conda activate tf-gpu-py37
+# module load python/3.7.4 cuda/10.0.130 cudnn/7.4 tensorflow/2.0.0_gpu_py37
+
+
+nfiles=100
+method='levy'
+python -u /users/afengler/git_repos/nn_likelihoods/keras_fit_model.py --machine x7 --method $method --nfiles $nfiles --datafolder /media/data_cifs/afengler/data/kde/${method}/training_data_binned_0_nbins_0_n_20000/ --nbydataset 10000000 --warmstart 0
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Pick number of files to consider
+# nfiles=100
+# method='levy'
+
+# # Function call
+# python -u keras_fit_model.py --machine x7 --method $method --nfiles $nfiles --datafolder /media/data_cifs/afengler/data/kde/${method}/training_data_binned_0_nbins_0_n_20000/ --nbydataset 10000000 --warmstart 0
