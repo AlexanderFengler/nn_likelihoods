82a4c373141d606680a89696072c8438cc1f4883
Alexander <alexanderfengler@gmx.de>
2020-04-01 18:37:53 -0400

    ..

--- sbatch_kde_to_base_simulations.sh
+++ sbatch_kde_to_base_simulations.sh
@@ -22,7 +22,7 @@
 # Run a command
 method='full_ddm'
 machine='ccv'
-nproc=2
+nproc=4
 python -u kde_train_test.py --machine $machine --method $method --simfolder training_data_binned_0_nbins_0_n_20000 --fileprefix ${method}_nchoices_2_train_data_binned_0_nbins_0_n_20000 --outfolder training_data_binned_0_nbins_0_n_20000 --nbyparam 1000 --mixture 0.8 0.1 0.1 --fileid $SLURM_ARRAY_TASK_ID --nproc $nproc
 
 #--fileid $SLURM_ARRAY_TASK_ID
