6854d2c0c39cbd4f48cb300578f6886d545e93c8
Alexander <alexanderfengler@gmx.de>
2020-04-02 00:44:57 -0400

    ..

--- sbatch_kde_to_base_simulations.sh
+++ sbatch_kde_to_base_simulations.sh
@@ -25,7 +25,7 @@
 method='full_ddm'
 machine='ccv'
 nproc=8
-python -u kde_train_test.py --machine $machine --method $method --simfolder training_data_binned_0_nbins_0_n_20000 --fileprefix ${method}_nchoices_2_train_data_binned_0_nbins_0_n_20000 --outfolder training_data_binned_0_nbins_0_n_20000 --nbyparam 1000 --mixture 0.8 0.1 0.1 --fileid $SLURM_ARRAY_TASK_ID --nproc $nproc
+python -u /users/afengler/git_repos/nn_likelihoods/kde_train_test.py --machine $machine --method $method --simfolder training_data_binned_0_nbins_0_n_20000 --fileprefix ${method}_nchoices_2_train_data_binned_0_nbins_0_n_20000 --outfolder training_data_binned_0_nbins_0_n_20000 --nbyparam 1000 --mixture 0.8 0.1 0.1 --fileid $SLURM_ARRAY_TASK_ID --nproc $nproc
 
 #--fileid $SLURM_ARRAY_TASK_ID
 #python -u /users/afengler/git_repos/nn_likelihoods/navarro_fuss_train_test.py
