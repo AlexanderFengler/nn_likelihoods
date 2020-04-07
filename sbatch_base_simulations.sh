bd243d3bc98b99d7cb4a0d94e6c24c02fe4441be
Alexander <alexanderfengler@gmx.de>
2020-04-07 18:57:36 -0400

    ..

--- sbatch_base_simulations.sh
+++ sbatch_base_simulations.sh
@@ -25,7 +25,7 @@
 #declare -a dgps=( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "lca" "race_model" "ddm_seq2" "ddm_par2" "ddm_mic2" "ddm_seq2_angle" "ddm_par2_angle" "ddm_mic2_angle") 
 declare -a dgps=( "ddm_seq2_angle" ) #  "ddm_mic2_angle" "ddm_par2_angle" ) 
 n_samples=( 100000 )   # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
-n_choices=( 2 ) #( 4 5 6 )
+n_choices=( 4 ) #( 4 5 6 )
 n_parameter_sets=10000   #20000
 n_bins=( 512 )
 binned=1
