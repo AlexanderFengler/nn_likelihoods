feac6a7d0df96989d16235088fccd782e331c932
Alexander <alexanderfengler@gmx.de>
2020-04-08 02:06:53 -0400

    ..

--- sbatch_base_simulations.sh
+++ sbatch_base_simulations.sh
@@ -17,21 +17,21 @@
 #SBATCH --mem=32G
 #SBATCH -c 14
 #SBATCH -N 1
-#SBATCH --array=1-1
+#SBATCH --array=1-300
 
 # --------------------------------------------------------------------------------------
 # Sequentially run different kind of models
 
-declare -a dgps=( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "levy" ) # "lca" "race_model" "ddm_seq2" "ddm_par2" "ddm_mic2" "ddm_seq2_angle" "ddm_par2_angle" "ddm_mic2_angle") 
-#declare -a dgps=( "ddm_seq2_angle" ) #  "ddm_mic2_angle" "ddm_par2_angle" ) 
-n_samples=( 1024 )   # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
-n_choices=( 2 ) #( 4 5 6 )
-n_parameter_sets=1000   #20000
-n_bins=( 0 )
-binned=0
+# declare -a dgps=( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "levy" ) # "lca" "race_model" "ddm_seq2" "ddm_par2" "ddm_mic2" "ddm_seq2_angle" "ddm_par2_angle" "ddm_mic2_angle") 
+declare -a dgps=( "ddm_mic2_angle" "ddm_par2_angle" ) # ( "ddm_seq2_angle" )  
+n_samples=( 100000 )   # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
+n_choices=( 4 ) #( 4 5 6 )
+n_parameter_sets=10000   #20000
+n_bins=( 512 )
+binned=1
 machine="ccv" #"ccv"
-datatype="parameter_recovery" #"cnn_train" # "parameter_recovery"
-mode="test"
+datatype="cnn_train" #"parameter_recovery" #"cnn_train" # "parameter_recovery"
+mode="cnn"
 maxt=20
 # outer -------------------------------------
 for bins in "${n_bins[@]}"
