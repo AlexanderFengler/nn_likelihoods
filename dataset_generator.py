3b92d3c05e9ba82ddafeacf84f82694c86f2c113
Alex <afengler@x9.clps.brown.edu>
2020-04-24 00:19:23 -0400

    ..

--- dataset_generator.py
+++ dataset_generator.py
@@ -475,9 +475,9 @@ class data_generator():
         for n in range(self.config['nparamsets']):
             for i in range(self.config['nsubjects']):
                 a, b = (np.array(params_lower_bnd) - global_means[n]) / global_stds[n], (np.array(params_upper_bnd) - global_means[n]) / global_stds[n]
-                subject_param_grid[n, i, :] = np.float32(global_means[n] + truncnorm.rvs(a, b, size = global_std.shape[1]) / global_stds[n])
+                subject_param_grid[n, i, :] = np.float32(global_means[n] + truncnorm.rvs(a, b, size = global_stds.shape[1]) / global_stds[n])
                 print(subject_param_grid[n, i, :])
-                
+
         return subject_param_grid, global_stds, global_means
 
 
