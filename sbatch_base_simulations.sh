b8dd71954801137bbbf47f30276429db4bdbc9f0
Alex <afengler@x9.clps.brown.edu>
2020-04-23 21:36:20 -0400

    ..

--- sbatch_base_simulations.sh
+++ sbatch_base_simulations.sh
@@ -22,8 +22,8 @@
 # --------------------------------------------------------------------------------------
 # Sequentially run different kind of models
 
-# declare -a dgps=( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "levy" ) # "lca" "race_model" "ddm_seq2" "ddm_par2" "ddm_mic2" "ddm_seq2_angle" "ddm_par2_angle" "ddm_mic2_angle") 
-declare -a dgps=( 'ddm' )    #( "ddm_mic2_angle" "ddm_par2_angle" ) # ( "ddm_seq2_angle" )  
+# declare -a dgps=( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "levy" ) # "lca" "race_model" "ddm_seq2" "ddm_par2" "ddm_mic2" "ddm_seq2_angle" "ddm_par2_angle" "ddm_mic2_angle")
+declare -a dgps=( "ddm" )  #( "ddm_mic2_angle" "ddm_par2_angle" ) # ( "ddm_seq2_angle" )
 n_samples=( 1024 )   # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
 n_choices=( 2 ) #( 4 5 6 )
 n_parameter_sets=100   #20000
