a9a50f57797c9af61f9e29a3bc2fd977669c7daa
Fengler <afengler@CLPS-AFENGLER.local>
2020-05-06 21:51:05 -0400

    ..

--- sbatch_keras_timings.sh
+++ sbatch_keras_timings.sh
@@ -26,5 +26,8 @@
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH --array=1-1
 
+source /users/afengler/.bashrc
+conda deactivate
+conda activate tf-gpu-py37
 
 python -u /users/afengler/git_repos/nn_likelihoods/keras_timing.py --machine ccv --nreps 200 --method ddm
