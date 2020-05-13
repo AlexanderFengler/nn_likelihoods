b323c335fce2bd1b5aebce14ac509b2c63127a05
Alexander Fengler <alexanderfengler@gmx.de>
2020-05-13 11:46:34 -0400

    ..

--- sbatch_train_mlp.sh
+++ sbatch_train_mlp.sh
@@ -37,7 +37,7 @@ machine='ccv'
 maxidfiles=300
 
 #!/bin/bash
-for i in {1..5}
+for i in {1..1}
 do
    echo "Now starting run: $i \n"
    python -u /users/afengler/git_repos/nn_likelihoods/keras_fit_model.py --machine $machine --method $method --nfiles $nfiles --maxidfiles $maxidfiles --datafolder /users/afengler/data/kde/${method}/training_data_binned_0_nbins_0_n_20000/ --nbydataset 10000000 --warmstart 0
