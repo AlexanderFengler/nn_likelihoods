af805f4416fa261db4b89dc18b4e788e80b0ecad
Alexander Fengler <alexanderfengler@gmx.de>
2020-05-14 13:01:51 -0400

    ..

--- .ipynb_checkpoints/sbatch_method_comparison-checkpoint.sh
+++ .ipynb_checkpoints/sbatch_method_comparison-checkpoint.sh
@@ -59,9 +59,9 @@ nmcmcsamples=20000
 nbyarrayjob=10
 nsamples=( 1024 2048 4096 ) # 2048 4096 ) #( 1024 2048 4096 )
 method='ddm_analytic'  #"full_ddm2"
-ids=( -1 )
+ids=( 1 2 3 )
 machine='ccv'
-analytic=1
+analytic=0
 #SLURM_ARRAY_TASK_ID=1
 
 for n in "${nsamples[@]}"
