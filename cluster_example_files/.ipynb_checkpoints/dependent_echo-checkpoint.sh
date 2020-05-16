ec26697aa7930e9f4fa04643345417de493d86e6
Alexander Fengler <alexanderfengler@gmx.de>
2020-05-16 00:32:56 -0400

    ..

--- cluster_example_files/.ipynb_checkpoints/dependent_echo-checkpoint.sh
+++ cluster_example_files/.ipynb_checkpoints/dependent_echo-checkpoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 my_fancy_var='hello'
-sbatch /users/afengler/git_repos/nn_likelihoods/sbatch_echo_env.sh
+sbatch /users/afengler/git_repos/nn_likelihoods/cluster_example_files/sbatch_echo_env.sh
