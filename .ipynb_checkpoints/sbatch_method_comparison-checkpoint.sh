14b9cee0537f4a792f0de23e1ed423cf9a230376
Alexander Fengler <alexanderfengler@gmx.de>
2020-06-04 00:03:51 -0400

    ..

--- .ipynb_checkpoints/sbatch_method_comparison-checkpoint.sh
+++ .ipynb_checkpoints/sbatch_method_comparison-checkpoint.sh
@@ -39,7 +39,7 @@ conda activate tf-cpu
 nmcmcsamples=25000
 nbyarrayjob=50
 ncpus=1
-nsamples=( 1024 ) #( 1024 2048 4096 ) # 2048 4096 ) #( 1024 2048 4096 )
+nsamples=( 4096 ) #( 1024 2048 4096 ) # 2048 4096 ) #( 1024 2048 4096 )
 method="ddm_analytic" #'ddm_sdv_analytic'   #'ddm_sdv_analytic'  #"full_ddm2"
 ids=( 2 )
 machine='ccv'
