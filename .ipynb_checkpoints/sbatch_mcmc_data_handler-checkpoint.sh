31a939cd0ff008b4983011f24b94feeb6148a295
Alexander Fengler <alexanderfengler@gmx.de>
2020-06-04 00:25:53 -0400

    ..

--- .ipynb_checkpoints/sbatch_mcmc_data_handler-checkpoint.sh
+++ .ipynb_checkpoints/sbatch_mcmc_data_handler-checkpoint.sh
@@ -19,9 +19,9 @@
 #SBATCH --array=1-1
 
 machine="ccv"
-ids=( -1 ) #( 0 1 2 3 4 5 6 7 8 9)
+ids=( 2 ) #( 0 1 2 3 4 5 6 7 8 9)
 ndata=( 1024 4096 ) #  4096 )
-method="ddm_sdv"
+method="ddm_sdv_analytic"
 analytic=0
 initmode='mle'
 
