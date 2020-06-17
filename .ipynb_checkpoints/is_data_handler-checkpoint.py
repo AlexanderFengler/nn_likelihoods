f97d5e8e92e8d46c49786a6b819bf4bb3cf6cb42
Alexander Fengler <alexanderfengler@gmx.de>
2020-06-17 00:45:16 -0400

    ..

--- .ipynb_checkpoints/is_data_handler-checkpoint.py
+++ .ipynb_checkpoints/is_data_handler-checkpoint.py
@@ -20,7 +20,7 @@ def collect_datasets_is(folder = [],
     
     
     
-    n_data_substring = 'n_' + str(ndata)
+    n_data_substring = 'N_' + str(ndata)
     
     is_dict = {}
     is_dict['gt'] = []
@@ -58,8 +58,8 @@ def collect_datasets_is(folder = [],
             is_dict['data'].append(param_recov_dat[1][0][idx, : , :])
         
             
-        print('Processed file: ', file_)
-        print(model + '_training_' in file_)
+            print('Processed file: ', file_)
+        #print(model + '_training_' in file_)
     
     is_dict['gt'] = np.stack(is_dict['gt'])
     is_dict['posterior_samples'] = np.stack(is_dict['posterior_samples'])
