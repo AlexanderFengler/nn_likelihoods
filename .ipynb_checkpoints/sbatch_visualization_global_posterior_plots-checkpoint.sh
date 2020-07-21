64e7d63279092b3bc9bc51a777124093fdc1e09b
Alexander Fengler <alexanderfengler@gmx.de>
2020-07-21 15:57:05 -0400

    ..

--- .ipynb_checkpoints/sbatch_visualization_global_posterior_plots-checkpoint.sh
+++ .ipynb_checkpoints/sbatch_visualization_global_posterior_plots-checkpoint.sh
@@ -3,7 +3,7 @@
 
 # plotlist=( "hdi_p" "hdi_coverage" "parameter_recovery_scatter" "model_uncertainty" "posterior_predictive" "posterior_pair") #( "posterior_variance" "hdi_coverage" "hdi_p" "parameter_recovery_scatter" "parameter_recovery_hist" "posterior_pair" "model_uncertainty" "posterior_predictive" )
 #plotlist=( "posterior_pair" "model_uncertainty" )
-plotlist=( "model_uncertainty" )
+plotlist=( "posterior_predictive" )
 # CNN
 
 # # DDM -----------------------------------------------
@@ -63,46 +63,46 @@ do
     python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
 done
 
-# # -------------------------------------------------------------
+# # # # -------------------------------------------------------------
 
-# # FULL_DDM2 ---------------------------------------------------
-# model="full_ddm2"
-# machine="home"
-# method="cnn"
-# traindattype="binned"
-# networkidx=-1
-# n=( 1024 4096 )
-# analytic=0
-# rhatcutoff=1.1
-# npostpred=9
-# npostpair=9
-# datafilter='none'
+# FULL_DDM2 ---------------------------------------------------
+model="full_ddm2"
+machine="home"
+method="cnn"
+traindattype="binned"
+networkidx=-1
+n=( 1024 4096 )
+analytic=0
+rhatcutoff=1.1
+npostpred=9
+npostpair=9
+datafilter='none'
 
-# for n_tmp in "${n[@]}"
-# do
-#     python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
-# done
+for n_tmp in "${n[@]}"
+do
+    python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
+done
+# -----------------------------------------------------------
+
+
+# # ORNSTEIN ---------------------------------------------------
+model="ornstein"
+machine="home"
+method="cnn"
+traindattype="binned"
+networkidx=-1
+n=( 4096 1024 )
+analytic=0
+rhatcutoff=1.1
+npostpred=9
+npostpair=9
+datafilter='none'
+
+for n_tmp in "${n[@]}"
+do
+    python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
+done
 # # -----------------------------------------------------------
-
-
-# # # ORNSTEIN ---------------------------------------------------
-# model="ornstein"
-# machine="home"
-# method="cnn"
-# traindattype="binned"
-# networkidx=-1
-# n=( 1024 4096 )
-# analytic=0
-# rhatcutoff=1.1
-# npostpred=9
-# npostpair=9
-# datafilter='none'
-
-# for n_tmp in "${n[@]}"
-# do
-#     python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
-# done
-# # # -----------------------------------------------------------
 
 
 # # LEVY ------------------------------------------------------------
@@ -126,25 +126,25 @@ done
 # done
 # # # -----------------------------------------------------------
 
-# # WEIBULL CDF 2 ---------------------------------------------------
-model="weibull_cdf"
-machine="home"
-method="cnn"
-traindattype="binned"
-networkidx=-1
-n=( 1024 4096 )
-analytic=0
-rhatcutoff=1.1
-npostpred=9
-npostpair=9
-datafilter='none'
+# # # WEIBULL CDF 2 ---------------------------------------------------
+# model="weibull_cdf"
+# machine="home"
+# method="cnn"
+# traindattype="binned"
+# networkidx=-1
+# n=( 1024 4096 )
+# analytic=0
+# rhatcutoff=1.1
+# npostpred=9
+# npostpair=9
+# datafilter='none'
 
-for n_tmp in "${n[@]}"
-do
-    python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
-done
+# for n_tmp in "${n[@]}"
+# do
+#     python -u visualization_global_posterior_plots.py --model $model --machine $machine --method $method --networkidx $networkidx --traindattype $traindattype --n $n_tmp --analytic $analytic --rhatcutoff $rhatcutoff --npostpred $npostpred --npostpair $npostpair --plots ${plotlist[@]} --datafilter $datafilter
+# done
 
-# -----------------------------------------------------------
+# # -----------------------------------------------------------
 
 
 # # # RACE 3 ---------------------------------------------------
@@ -363,7 +363,7 @@ done
 # method="mlp"
 # traindattype="kde"
 # networkidx=-1
-# n=( 4096 )
+# n=( 1024 4096 )
 # analytic=0
 # rhatcutoff=1.1
 # npostpred=9
