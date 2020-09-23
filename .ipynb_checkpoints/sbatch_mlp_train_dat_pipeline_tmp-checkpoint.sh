86e4cec803d8a5182bb7d2f7785668d00074eeb9
Alexander Fengler <alexanderfengler@gmx.de>
2020-09-23 00:23:25 -0400

    ..

--- .ipynb_checkpoints/sbatch_mlp_train_dat_pipeline_tmp-checkpoint.sh
+++ .ipynb_checkpoints/sbatch_mlp_train_dat_pipeline_tmp-checkpoint.sh
@@ -13,20 +13,20 @@
 #SBATCH --output /users/afengler/batch_job_out/tpl_1_%A_%a.out
 
 # Request runtime, memory, cores
-#SBATCH --time=12:00:00
-#SBATCH --mem=96G
-#SBATCH -c 14
+#SBATCH --time=18:00:00
+#SBATCH --mem=128G
+#SBATCH -c 12
 #SBATCH -N 1
-#SBATCH --array=1-300
+#SBATCH --array=1-10
 
 # --------------------------------------------------------------------------------------
 # Sequentially run different kind of models
 
 #declare -a dgps=( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "lca" "race_model" "ddm_seq2" "ddm_par2" "ddm_mic2" "ddm_seq2_angle" "ddm_par2_angle" "ddm_mic2_angle") 
-declare -a dgps=( "weibull_cdf2" )  # ( "angle2" )  #( "weibull_cdf" ) # ( "ddm_seq2_angle" "ddm_mic2_angle" "ddm_par2_angle" )
-n_samples=( 20000 )   # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
+declare -a dgps=( "ddm_sdv_elife" )  # ( "angle2" )  #( "weibull_cdf" ) # ( "ddm_seq2_angle" "ddm_mic2_angle" "ddm_par2_angle" )
+n_samples=( 100000 )   # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
 n_choices=( 2 ) #( 4 5 6 )
-n_parameter_sets=10000   #20000
+n_parameter_sets=2500   #20000
 n_bins=( 0 )
 binned=0
 machine="ccv" #"ccv"
@@ -53,17 +53,74 @@ do
                     do
                        echo "$dgp"
                        echo $n_c
-                       python -u dataset_generator.py --machine $machine --dgplist $dgp --datatype $datatype --nreps 1 --binned $binned --nbins $bins --maxt $maxt --nchoices $n_c --nsamples $n --mode $mode --nparamsets $n_parameter_sets --save 1 --deltat 0.001 --fileid $SLURM_ARRAY_TASK_ID 
-                       python -u simulator_get_stats.py --machine $machine --method $dgp --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} --fileprefix ${dgp}_nchoices_${n_c}_train_data_binned_${binned}_nbins_${bins}_n_${n} --fileid $SLURM_ARRAY_TASK_ID
-                       python -u kde_train_test.py --machine $machine --method $dgp --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} --fileprefix ${dgp}_nchoices_${n_c}_train_data_binned_${binned}_nbins_${bins}_n_${n} --outfolder training_data_binned_${binned}_nbins_${bins}_n_${n} --nbyparam $nbyparam --mixture 0.8 0.1 0.1 --fileid $SLURM_ARRAY_TASK_ID --nproc $nproc --analytic $analytic
+                       python -u dataset_generator.py --machine $machine \
+                                                      --dgplist $dgp \
+                                                      --datatype $datatype \
+                                                      --nreps 1 \
+                                                      --binned $binned \
+                                                      --nbins $bins \
+                                                      --maxt $maxt \
+                                                      --nchoices $n_c \
+                                                      --nsamples $n \
+                                                      --mode $mode \
+                                                      --nparamsets $n_parameter_sets \
+                                                      --save 1 \
+                                                      --deltat 0.001 \
+                                                      --fileid $SLURM_ARRAY_TASK_ID 
+                       
+                       python -u simulator_get_stats.py --machine $machine \
+                                                        --method $dgp \
+                                                        --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                        --fileprefix ${dgp}_nchoices_${n_c}_train_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                        --fileid $SLURM_ARRAY_TASK_ID
+                       
+                       python -u kde_train_test.py --machine $machine \
+                                                   --method $dgp \
+                                                   --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                   --fileprefix ${dgp}_nchoices_${n_c}_train_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                   --outfolder training_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                   --nbyparam $nbyparam \
+                                                   --mixture 0.8 0.1 0.1 \
+                                                   --fileid $SLURM_ARRAY_TASK_ID \
+                                                   --nproc $nproc \
+                                                   --analytic $analytic
            
                 done
             else
                  echo "$dgp"
                  echo ${n_choices[0]}
-                 python -u dataset_generator.py --machine $machine --dgplist $dgp --datatype $datatype --nreps 1 --binned $binned --nbins $bins --maxt $maxt --nchoices ${n_choices[0]} --nsamples $n --mode $mode --nparamsets $n_parameter_sets --save 1  --deltat 0.001 --fileid $SLURM_ARRAY_TASK_ID
-                 python -u simulator_get_stats.py --machine $machine --method $dgp --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} --fileprefix ${dgp}_nchoices_${n_choices[0]}_train_data_binned_${binned}_nbins_${bins}_n_${n} --fileid $SLURM_ARRAY_TASK_ID
-                 python -u kde_train_test.py --machine $machine --method $dgp --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} --fileprefix ${dgp}_nchoices_${n_choices[0]}_train_data_binned_${binned}_nbins_${bins}_n_${n} --outfolder training_data_binned_${binned}_nbins_${bins}_n_${n} --nbyparam $nbyparam --mixture 0.8 0.1 0.1 --fileid $SLURM_ARRAY_TASK_ID --nproc $nproc --analytic $analytic
+                 python -u dataset_generator.py --machine $machine \
+                                                --dgplist $dgp \
+                                                --datatype $datatype \
+                                                --nreps 1 \
+                                                --binned $binned \
+                                                --nbins $bins \
+                                                --maxt $maxt \
+                                                --nchoices ${n_choices[0]} \
+                                                --nsamples $n \
+                                                --mode $mode \
+                                                --nparamsets $n_parameter_sets \
+                                                --save 1 \
+                                                --deltat 0.001 \
+                                                --fileid $SLURM_ARRAY_TASK_ID
+                                                
+                 
+                 python -u simulator_get_stats.py --machine $machine \
+                                                  --method $dgp \
+                                                  --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                  --fileprefix ${dgp}_nchoices_${n_choices[0]}_train_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                                  --fileid $SLURM_ARRAY_TASK_ID
+                                                  
+                 python -u kde_train_test.py --machine $machine \
+                                             --method $dgp \
+                                             --simfolder training_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                             --fileprefix ${dgp}_nchoices_${n_choices[0]}_train_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                             --outfolder training_data_binned_${binned}_nbins_${bins}_n_${n} \
+                                             --nbyparam $nbyparam \
+                                             --mixture 0.8 0.1 0.1 \
+                                             --fileid $SLURM_ARRAY_TASK_ID \
+                                             --nproc $nproc \
+                                             --analytic $analytic
             fi
         done
                 # normal call to function
