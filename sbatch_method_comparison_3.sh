ff43959574c2f2f1f93cb6a837fc9da3ca27e6c5
Alexander Fengler <alexanderfengler@gmx.de>
2020-09-30 01:56:45 -0400

    ..

--- sbatch_method_comparison_3.sh
+++ sbatch_method_comparison_3.sh
@@ -3,7 +3,7 @@
 # Default resources are 1 core with 2.8GB of memory per core.
 
 # job name:
-#SBATCH -J ddm_kde_1
+#SBATCH -J ddm_a_a_4
 
 # priority
 ##SBATCH --account=bibs-frankmj-condo
@@ -11,15 +11,15 @@
 ##SBATCH --account=bibs-frankmj-condo
 
 # output file
-#SBATCH --output /users/afengler/batch_job_out/mc_ddm_kde_1_%A_%a.out
+#SBATCH --output /users/afengler/batch_job_out/mc_ddm_a_a_4_%A_%a.out
 
 # Request runtime, memory, cores:
-#SBATCH --time=30:00:00
-#SBATCH --mem=64G
-#SBATCH -c 10
+#SBATCH --time=24:00:00
+#SBATCH --mem=16G
+#SBATCH -c 1
 #SBATCH -N 1
 ##SBATCH -p gpu --gres=gpu:1
-#SBATCH --array=1-20
+#SBATCH --array=1-100
 
 # Run a command
 #source /users/afengler/miniconda3/etc/profile.d/conda.sh
@@ -33,25 +33,44 @@ conda activate tf-cpu
 # conda deactivate
 # conda activate tf-gpu-py37
 
-
 # NNBATCH RUNS
 
-nmcmcsamples=25000
-nbyarrayjob=50
-ncpus=8
-nsamples=( 4096 ) # 2048 4096 ) #( 1024 2048 4096 )
-method='ddm_analytic'   #'ddm_sdv_analytic'  #"full_ddm2"
-ids=( 2 )
+nmcmcsamples=2000
+nbyarrayjob=10
+ncpus=1
+nsamples=( 4096 ) #( 1024 2048 4096 ) # 2048 4096 ) #( 1024 2048 4096 )
+method="ddm" #'ddm_sdv_analytic'   #'ddm_sdv_analytic'  #"full_ddm2"
+modelidentifier=None
+ids=( -1 )
 machine='ccv'
 samplerinit='mle'
-analytic=1
+outfilesignature='elife_slice_'
+infilesignature='elife_'
+sampler='slice'
+analytic=0
 #SLURM_ARRAY_TASK_ID=1
 
 for n in "${nsamples[@]}"
 do
     for id in "${ids[@]}"
     do 
-        python -u method_comparison_sim.py --machine $machine --method $method --nsamples $n --nmcmcsamples $nmcmcsamples --datatype parameter_recovery --sampler diffevo --infileid 1 --outfilesig _expanded_bounds_test --outfileid $SLURM_ARRAY_TASK_ID --activedims 0 1 2 3 4 --samplerinit $samplerinit --ncpus $ncpus --nbyarrayjob $nbyarrayjob --nnbatchid $id --analytic $analytic
+        python -u method_comparison_sim.py --machine $machine \
+                                                      --method $method \
+                                                      --modelidentifier $modelidentifier \
+                                                      --nsamples $n \
+                                                      --nmcmcsamples $nmcmcsamples \
+                                                      --datatype parameter_recovery \
+                                                      --sampler $sampler \
+                                                      --infilesignature $infilesignature  \
+                                                      --outfileid $SLURM_ARRAY_TASK_ID \
+                                                      --activedims 0 1 2 3 4 5 6 \
+                                                      --samplerinit $samplerinit \
+                                                      --ncpus $ncpus \
+                                                      --nbyarrayjob $nbyarrayjob \
+                                                      --nnbatchid $id \
+                                                      --analytic $analytic \
+                                                      --outfilesig $outfilesignature
     done
 done
 
+# --outfileid $SLURM_ARRAY_TASK_ID
