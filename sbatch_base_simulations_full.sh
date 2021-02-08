a91a44e3cad5e7d68ae82b6145a233c766cc99ce
Alexander Fengler <alexanderfengler@gmx.de>
2021-02-08 17:38:10 -0500

    ..

--- sbatch_base_simulations_full.sh
+++ sbatch_base_simulations_full.sh
@@ -24,7 +24,6 @@
 # Sequentially run different kind of models
 declare -a dgps=( "" ) # "race_model" "lca" ) #"ddm_sdv_analytic" "ddm_sdv_red_analytic" ) #( "ddm" "full_ddm" "angle" "weibull_cdf" "ornstein" "levy" )  #( "ddm_mic2_angle" "ddm_par2_angle" ) # ( "ddm_seq2_angle" )
 n_samples=( 10000 )  # ( 128 256 512 1024 2048 4096 8192 50000 100000 200000 400000 )
-n_choices=( 3 ) #( 4 5 6 )
 nparamsets=5000
 nparamsetsrej=20 # cnn 20000 but 150 array   # mlp 10000 but 300 array # KRISHN: 10
 n_bins=( 0 ) # KRISHN: n_bins=0
@@ -51,7 +50,6 @@ do
                                                        --nreps 1 \
                                                        --nbins $bins \
                                                        --maxt $maxt \
-                                                       --nchoices ${n_choices[0]} \
                                                        --nsamples $n \
                                                        --nparamsets $nparamsets \
                                                        --nparamsetsrej $nparamsetsrej \
