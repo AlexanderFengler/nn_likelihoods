1f8565d8633c73ac8778e1d9a1738377ad860ffd
Alexander Fengler <alexanderfengler@gmx.de>
2021-02-08 19:06:44 -0500

    ..

---
+++ sbatch_train_mlp_cpu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Default resources are 1 core with 2.8GB of memory per core.
+
+# job name:
+#SBATCH -J mlp_analytic
+
+# priority
+#SBATCH --account=carney-frankmj-condo
+
+# email error reports
+#SBATCH --mail-user=alexander_fengler@brown.edu 
+#SBATCH --mail-type=ALL
+
+# output file
+#SBATCH --output /users/afengler/batch_job_out/mlp_train_ddm_analytic_%A_%a.out
+
+# Request runtime, memory, cores
+#SBATCH --time=18:00:00
+#SBATCH --mem=16G
+#SBATCH -c 8
+#SBATCH -N 1
+#SBATCH --array=1-1
+
+nfiles=10 # Number of training data files to use for training
+n_networks_to_train=5 # Number of networks to train
+method='test' # choose data generating process (includes analytic label !) 
+#analytic=0 # Training labels from analytic likelihood (1) or from KDE (0) (This is now all in the model_name)
+machine='af_ccv' # Choose machine
+train_n_sim=10000 # Choose training data --> based on how many simulations ?
+training_data_folder="training_data_binned_0_nbins_0_n_${train_n_sim}/"
+
+for ((i = 1; i <= $n_networks_to_train; i++))
+    do
+       echo "NOW TRAINING NETWORK: $i of $n_networks_to_train"
+       python -u /users/afengler/git_repos/nn_likelihoods/keras_fit_model.py --machine $machine \
+                                                                             --method $method \
+                                                                             --nfiles $nfiles \
+                                                                             --traindatafolder $training_data_folder \
+                                                                             --warmstart 0 
+                                                                             #--analytic $analytic
+    done
